9a27ecd3dacd3905e037309f02238bf0ec917874
Duy Dien Nguyen <diennd.vnu@gmail.com>
2025-01-21 15:54:27 +0700

    aaaaa

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import receive_gps_data, receive_rfid_data
 
-urlpatterns are [
+urlpatterns = [
     path('receive-gps/', receive_gps_data, name='receive_gps_data'),
     path('receive-rfid/', receive_rfid_data, name='receive_rfid_data'),
 ]
