17ee780ecbf0c8ad4307800621f49fe78dd5c3b6
Duy Dien Nguyen <diennd.vnu@gmail.com>
2025-02-04 11:15:03 +0700

    ABCDEFGH

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
 import json
-from .models import GPSData
+from .models import GPSData, RFIDData
 import logging
 
 logger = logging.getLogger(__name__)
@@ -22,6 +22,7 @@ def receive_gps_data(request):
                 speed=data['speed']
             )
             gps_data.save()
+            logger.info("GPS data saved successfully")
             return JsonResponse({'status': 'success', 'data': data}, status=200)
         except json.JSONDecodeError:
             logger.error("Invalid JSON received")
@@ -33,12 +34,15 @@ def receive_rfid_data(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
+            logger.info(f"Received RFID data: {data}")
             rfid_data = RFIDData(
                 device_id=data['device_id'],
                 UID=data['UID']
             )
             rfid_data.save()
+            logger.info("RFID data saved successfully")
             return JsonResponse({'status': 'success', 'data': data}, status=200)
         except json.JSONDecodeError:
+            logger.error("Invalid JSON received")
             return JsonResponse({'status': 'error', 'message': 'Invalid JSON'}, status=400)
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
