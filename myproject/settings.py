f14851f28f86055010a366d820b5b420b62468c3
Duy Dien Nguyen <diennd.vnu@gmail.com>
2025-02-04 11:59:20 +0700

    ABC

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ga5uqb3lweyiobylh7-p5*^ji_%z2sv!qcvumvo#3f8^+eg*__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGGING = {
     'version': 1,
@@ -44,7 +47,7 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = ['test-bus-api.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['diennd25.pythonanywhere.com']
 
 
 # Application definition
