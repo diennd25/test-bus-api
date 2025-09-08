3d333e16cfd23c8fe87a5bb569dc80c0f23f09e8
diennd25 <Dien.Nguyenduy@amkor.com>
2025-09-08 09:09:18 +0700

    Update wsgi.py

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -2,15 +2,10 @@ import os
 import sys
 
 # Add your project directory to the sys.path
-sys.path.append('/home/diennd25/test-bus-api')
+sys.path.append('/root/Downloads/test-bus-api')  # Update this to match your actual path
 
 # Set the Django settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
-
-# Activate your virtual environment
-activate_this = '/home/diennd25/myenv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
 
 # Import the WSGI application
 from django.core.wsgi import get_wsgi_application
